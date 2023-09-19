fb8642952b74e5b6ed79735255e896055b683188
joceamayar <jocelin@tavlee.com>
2023-09-19 18:48:01 -0400

    get and put are now working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,31 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  console.log('GET all from the database');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (id, content) => {
+  console.error('putDb not implemented');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, todo: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 initdb();
